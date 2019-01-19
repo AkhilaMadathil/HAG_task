6aec3c62ff50723a377910c17e3e80c4a3937729
Akhila Madathil <madathilakhila@gmail.com>
2019-01-19 21:50:04 +0530

    pod changes

---
+++ hagTask/hagTask/DescriptionPageViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DescriptionPageViewController.swift
+//  hagTask
+//
+//  Created by MAKL on 19/01/19.
+//  Copyright © 2019 Akhila. All rights reserved.
+//
+
+import UIKit
+import Parchment
+
+class DescriptionPageViewController: UIViewController, PagingViewControllerDataSource  {
+    func numberOfViewControllers<T>(in pagingViewController: PagingViewController<T>) -> Int where T : PagingItem, T : Comparable, T : Hashable {
+        return 2
+    }
+    
+    func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, viewControllerForIndex index: Int) -> UIViewController where T : PagingItem, T : Comparable, T : Hashable {
+        
+    }
+    
+    func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, pagingItemForIndex index: Int) -> T where T : PagingItem, T : Comparable, T : Hashable {
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
